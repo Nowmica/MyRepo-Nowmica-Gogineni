8375b00383271913db6e287b9a6d3fd2f08c391b
Nowmica <57382951+Nowmica@users.noreply.github.com>
2020-04-21 17:43:29 +0530

    Update Main.java

--- Assignment-1/Case 5/Main.java
+++ Assignment-1/Case 5/Main.java
@@ -1,28 +1,48 @@
-//different ways to initialize private data member variables
-package mypack;
+package com.hcl.domino.assig1;
 
-public class Main
+/**
+ * This program illustrates the different ways of initializing the private member variables
+ */
+public class PrivateMem 
 {
 
 	public static void main(String[] args) 
 	{
-		
+		if(args.length==0)
+		{
+		System.out.println("Usage description of the program\nThe program is used for displaying the length,width and height of a triangle\nNeed a input value to display\ne.g. java Main 2 5");
+		}
+		else
+		{
+		try
+		{
 		//creating object for the class and initializing through constructor
-		Main obj=new Main(20);
-		
+		PrivateMem obj=new PrivateMem(Integer.parseInt(args[0]));
 		//setting the value through object
-		obj.setLength(10);
+		obj.setLength(Integer.parseInt(args[1]));
 		obj.display();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Invalid input");
+		}
+		
+	}
 	}
 	
-	//default constructor
-	Main()
+	/**
+	 * This is a default constructor
+	 */
+	PrivateMem()
 	{
 		
 	}
 	
-	//Argumented constructor
-	Main(int number)
+	/**
+	 * This is a parameterized constructor
+	 * @param number
+	 */
+	PrivateMem(int number)
 	{
 		this.height=number;
 	}
@@ -40,10 +60,20 @@ public class Main
 	 public int getLength() {
 		return length;
 	}
+	 
+	 /**
+	    * This is a setter method which is used to set the value of length
+	    * @param lenght
+	    * @return null
+	    */
 	public void setLength(int length) {
 		this.length = length;
 	}
 	
+	/**
+	    * This is the display method which is used to display the values of length,width,height
+	    * @return null
+	    */
 	private void display()
 	 {
 		 System.out.println("The length,height and width is "+length+ " " +height+ " " +width);
