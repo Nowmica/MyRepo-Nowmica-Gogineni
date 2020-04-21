3bfda2d6e92e434f4aa6c1bccff1f2e8ccd12e8d
Nowmica <57382951+Nowmica@users.noreply.github.com>
2020-04-21 14:52:42 +0530

    Update Main.java

--- Assignment-1/Case 1/Main.java
+++ Assignment-1/Case 1/Main.java
@@ -1,8 +1,20 @@
-package mypack;
+package com.hcl.domino.assign1;
 
+/*
+ This class is created for checking the data type and size of the input  given by the user.
+ The size and data type are defined by using some methods in java.lang package.
+ */
 public class Main 
 {	
 public static void main(String[] args) 
+{
+	
+//Displaying the usage of the program to the user.	
+if(args.length==0)
+{
+System.out.println("Usage description of the program\n\nThe program is used for finding the datatype and size of the input\nNeed a input value to determine it\ne.g. java Main 2 dsjj .1");
+}
+else
 {
 for(int i=0; i<args.length;i++)
 {   
@@ -28,30 +40,43 @@ NumberOrNot(args[i]);
 }
 }
 }
-		
-//Method to check for number data type
+}		
+/* 
+This is the method used for checking for number data type i.e, int,long,float and double.
+The method takes string as a parameter and checks for the data type.
+Exceptional Handling mechanism is used for checking the data type.
+some methods from java.lang package are used for converting the string to the primitive data type i.e, Integer.parseInt()
+ */
 static void NumberOrNot(String str)
 {
 try
 {
+	
+//checking for integer data type
 System.out.println(Integer.parseInt(str) +" integer "+(Integer.BYTES));
 }
 catch(Exception ex)
 {
 try
 {
+	
+//checking for Long data type
 System.out.println(Long.parseLong(str) +" long "+(Long.BYTES));
 }
 catch(Exception ex1)
 {
 try
 {
+	
+//checking for float data type
 System.out.println(Float.parseFloat(str)+" Float "+(Float.BYTES));
 }
 catch(Exception ex2)
 {
 try
 {
+
+//checking for double data type
 System.out.println(Double.parseDouble(str)+" Double "+(Double.BYTES));
 }	
 catch(Exception e)
